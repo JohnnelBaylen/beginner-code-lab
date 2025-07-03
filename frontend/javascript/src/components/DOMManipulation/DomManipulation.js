af86de7d1bff097d3b0143de0d6b59769a8e8e54
JohnnelBaylen <jnelbaylen@gmail.com>
2025-07-04 01:21:28 +0800

    chore: add folder and implement DOM manipulation

---
+++ frontend/javascript/src/components/DOMManipulation/DomManipulation.js
@@ -0,0 +1,8 @@
+function onButton(){
+    alert("On Alert");
+}
+
+const offButton = document.getElementById('offButton');
+offButton.addEventListener("click", function(){
+    alert("Off Alert");
+});
