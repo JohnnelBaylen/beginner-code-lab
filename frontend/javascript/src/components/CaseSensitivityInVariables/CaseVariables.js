f53f5e293fa628bdf2f6e107ae56a5466735aeac
JohnnelBaylen <jnelbaylen@gmail.com>
2025-07-04 01:18:21 +0800

    chore: add case variables

---
+++ frontend/javascript/src/components/CaseSensitivityInVariables/CaseVariables.js
@@ -0,0 +1,15 @@
+
+// Camel Case
+const camelCase = "thisISCamelCase";
+
+// Snake Case
+const snake_case = "THIS_IS_SNAKE_CASE";
+
+// Pascal Case 
+const PascalCase = "ThisIsPascalCASE";
+
+// Kebab Case
+const kebabcase = "this-is-kebab-case";
+
+console.log(camelCase, snake_case, PascalCase, kebabcase);
+
