845044a96e40bb63a94bf508fdf1b99f399bb500
JohnnelBaylen <jnelbaylen@gmail.com>
2025-07-06 21:09:42 +0800

    cpp: add variables and print data types

---
+++ backend/cpp/src/components/Variables/DataTypes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    string fullName = "John Doe";
+    char sectionChar = 'A';
+    int  numberAge = 19;
+    bool height = true;
+    float GWA = 95.00;
+    double decimal = 1.5;
+
+    cout << "I'm " << fullName << endl; 
+    cout << "Blood Type : " << sectionChar << endl;
+    cout << "Age " << numberAge << endl;
+    cout << "Is Tall " << height << endl; 
+    cout << "Average : " << GWA << endl;
+    cout << "Convert : " << decimal;
+
+    return 0;
+}
