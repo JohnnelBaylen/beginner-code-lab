836ef72d1210b8f52bf5f0e65dbd4a7289bdff51
JohnnelBaylen <jnelbaylen@gmail.com>
2025-07-14 10:31:16 +0800

    chore: add message for arrow functions

---
+++ frontend/javascript/src/functions/arrow.js
@@ -0,0 +1,6 @@
+const message = () => {
+    let success = "Embrace challenges as opportunities, maintain a relentless focus on your goals, and never underestimate the power of perseverance.";
+    console.log(success);
+}
+
+message();
