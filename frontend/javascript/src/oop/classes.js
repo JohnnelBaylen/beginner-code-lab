f10dedffad3d53b6083ae69f5595e7a5942fd04b
JohnnelBaylen <jnelbaylen@gmail.com>
2025-07-17 20:59:42 +0800

    chore: add classes and objects

---
+++ frontend/javascript/src/oop/classes.js
@@ -0,0 +1,10 @@
+class programmingLanguage {
+    constructor(name, year){
+        this.name =  name;
+        this.year = year;
+    }
+}
+
+const Founder = new programmingLanguage("Brendan Eich", 1995);
+
+console.log(Founder);
