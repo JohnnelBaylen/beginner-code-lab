e1f9f0a82523645e991a395426486f6eac06806d
JohnnelBaylen <jnelbaylen@gmail.com>
2025-07-25 19:52:51 +0800

    chore: add data types to config folder

--- backend/rust/src/config/datatypes.rs
+++ backend/rust/src/config/datatypes.rs
@@ -1,3 +1,14 @@
 fn main(){
-    
+
+    let _my_age: i32 = 19;
+    let _my_gwa: f64 = 1.50;
+    let _my_section: char = 'A';
+    let _my_full_name: &str = "John Doe";
+    let _is_tall: bool  = true;
+
+    print!("\n{}", _my_age);
+    print!("\n{}", _my_gwa);
+    print!("\n{}", _my_section);
+    print!("\n{}", _my_full_name);
+    print!("\n{}", _is_tall);
 }
