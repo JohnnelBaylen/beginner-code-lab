8130550cf0315d803c1da4f2676dcf8510ce84f2
JohnnelBaylen <jnelbaylen@gmail.com>
2025-08-06 19:41:29 +0800

    feat: add queue

---
+++ backend/java/dsa/queue/QueueExample.java
@@ -0,0 +1,15 @@
+package dsa.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueExample {
+    public static void main(String[] args) {
+
+        // Create a Queue of Integers using LinkedList
+        Queue<Integer> q = new LinkedList<>();
+
+        // Displaying the Queue
+        System.out.println("Queue elements: " + q);
+    }
+}
