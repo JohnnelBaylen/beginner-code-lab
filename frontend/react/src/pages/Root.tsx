9ba45ccbaf98bd2da84e3259b78bc379759bbab8
JohnnelBaylen <jnelbaylen@gmail.com>
2025-08-15 12:35:23 +0800

    chore: update import

--- frontend/react/src/pages/Root.tsx
+++ frontend/react/src/pages/Root.tsx
@@ -1,10 +1,13 @@
-import Card from '@components/Card';
+import Navbar from '@components/Navbar';
+
 
 export default function Root(){
     return(
         <>
-            <Card title="Characters Name" content="This is the card content." />
+            <Navbar/>    
+
         </>
+
     );
 }
 
