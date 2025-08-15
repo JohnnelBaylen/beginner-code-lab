acae32e0498a67181a68e94787ed93307731469b
JohnnelBaylen <jnelbaylen@gmail.com>
2025-08-15 12:38:04 +0800

    chore: install lucide and tailwind

--- frontend/react/src/main.tsx
+++ frontend/react/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@styles/index.css'
+import '@styles/global.css'
 import Root from '@pages/Root.tsx'
 
 createRoot(document.getElementById('root')!).render(
--- frontend/react/src/pages/Root.tsx
+++ frontend/react/src/pages/Root.tsx
@@ -7,7 +7,6 @@ export default function Root(){
             <Navbar/>    
 
         </>
-
     );
 }
 
