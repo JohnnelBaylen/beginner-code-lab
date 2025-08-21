54618bb6bc0d157cbd9575f570288c44672aeddc
JohnnelBaylen <jnelbaylen@gmail.com>
2025-08-21 11:19:07 +0800

    chore: add number, string, and boolean

---
+++ frontend/typescript/src/basics/variables.ts
@@ -0,0 +1,15 @@
+let greetings: string = "Hello, John";
+greetings.toLowerCase();
+
+console.log(greetings);
+
+let SCHOOL_ID: number = 202500720;
+SCHOOL_ID.toFixed();
+
+console.log(SCHOOL_ID);
+
+let isTall: boolean = true;
+isTall.valueOf();
+
+console.log(isTall);
+ 
