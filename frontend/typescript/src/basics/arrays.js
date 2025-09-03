f180e1ea198d7715e8918d2e8a588b0ea5b8fe30
JohnnelBaylen <jnelbaylen@gmail.com>
2025-09-03 08:03:48 +0800

    chore: add array content

---
+++ frontend/typescript/src/basics/arrays.js
@@ -0,0 +1,7 @@
+var names = [];
+names.push("John");
+names.push("Jack");
+console.log(names);
+var numbers = [1, 2, 3];
+numbers.push(3);
+console.log(numbers);
---
+++ frontend/typescript/src/basics/arrays.ts
@@ -0,0 +1,10 @@
+const names: string[] = []
+names.push("John")
+names.push("Jack")
+
+console.log(names)
+
+const numbers = [1, 2, 3]
+numbers.push(3)
+
+console.log(numbers)
